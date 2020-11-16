8d7ca2583bd1c36b59ca252c836be691f4a9dd32
Mateo <m.narvaez2207@gmail.com>
2020-11-16 17:14:37 +0000

    I forgot to submit it in the previous commit

---
+++ resources/js/components/products.vue
@@ -0,0 +1,37 @@
+<template>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Type</th>
+        <th>Description</th>
+        <th>price</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="product in products" :key="product.id">
+        <td>{{ product.name }}</td>
+        <td>{{ product.tyoe }}</td>
+        <td>{{ product.description }}</td>
+        <td>{{ product.price }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+
+  mounted() {
+    if (this.$root.products.length === 0) {
+      axios.get('api/products')
+        .then(response => {
+          this.$root.products = response.data.data
+          this.products = this.$root.products
+        })
+    } else {
+      this.products = this.$root.products
+    }
+  },
+};
+</script>

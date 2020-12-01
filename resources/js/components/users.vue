333e1848315afcf472f365b5a3f69653c3529522
Mateo <m.narvaez2207@gmail.com>
2020-12-01 13:11:06 +0000

    Update User component (images in the list)
    Updated the list of user component, now it has images and added the add user button with no functions

--- resources/js/components/users.vue
+++ resources/js/components/users.vue
@@ -1,35 +1,55 @@
 <template>
-<v-main class="grey lighten-4">
-    <br>
+  <v-main class="grey lighten-4">
+    <br />
     <!--<div class="title2">User List</div> -->
-    
+
     <v-container fluid>
-        
-        <div class="white rounded-lg px-5 py-5 my-5">
-            <div class="text-h2  font-weight-bold">List Users</div>
-            <br>
-            <v-card>
-                <v-card-title>
-                    <v-text-field
-                        v-model="search"
-                        append-icon="mdi-magnify"
-                        label="Search"
-                        single-line
-                        hide-details 
-                    ></v-text-field>
-                </v-card-title>
-                <v-data-table
-                    :headers="headers"
-                    :items="users"
-                    :search="search"
-                    show-group-by
-                    class="elevation-1"
-                >
-                </v-data-table>
-            </v-card>
+      <div class="white rounded-lg px-5 py-5 my-5">
+        <div class="text-h2 font-weight-bold">
+          Users List
+          <div class="float-right">
+          <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+            New User
+          </v-btn>
+          </div>
         </div>
+        <br />
+        <v-card>
+          <v-card-title>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table
+            :headers="headers"
+            :items="users"
+            :search="search"
+            show-group-by
+            multi-sort
+            class="elevation-1"
+          >
+            <template v-slot:item.photo_url="{ item }">
+              <v-img
+                class="border-card"
+                :src="`/storage/fotos/${item.photo_url}`"
+                max-height="90"
+                max-width="80"
+                position="center"
+                sizes=""
+              ></v-img>
+            </template>
+          </v-data-table>
+        </v-card>
+      </div>
     </v-container>
-</v-main>
+  </v-main>
 </template>
 
 <script>
@@ -38,19 +58,19 @@ export default {
   data: function () {
     return {
       search: "",
-      headers:[
-      {
-        text: 'Name',
-        align: 'start',
-        value: 'name',
-        groupable: false
-      },
-      {text: 'Email', value: 'email', groupable: false},
-      {text: 'Type', value: 'type'},
-      {text: 'Photo', value: 'photo_url', groupable: false}          
+      headers: [
+        {
+          text: "Photo",
+          value: "photo_url",
+          groupable: false,
+          sortable: false,
+        },
+        { text: "Id", value: "id", groupable: false },
+        { text: "Name", value: "name", groupable: false },
+        { text: "Email", value: "email", groupable: false },
+        { text: "Type", value: "type" },
       ],
-      
-    }
+    };
   },
   methods: {
     getUsers() {
@@ -67,10 +87,11 @@ export default {
 </script>
 
 <style>
-  .title2{
-    margin-left: 10%;
-    font-size: 40px;
-    margin-bottom: -2%;
-    font-weight: bold;
-  }
+.border-card {
+  border-radius: 50%;
+  min-height: 70px;
+  min-width: 70px;
+  margin-top: 5%;
+  margin-bottom: 5%;
+}
 </style>
